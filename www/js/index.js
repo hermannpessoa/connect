ceeb57750a450b2ba2257718876728fe0a1a99ce
hermannpessoa <performaweb@Performa-Printer.local>
2015-07-19 17:29:01 -0300

    script to connect

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        checkConnection();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -47,3 +48,20 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    alert('Connection type: ' + states[networkState]);
+}
+
